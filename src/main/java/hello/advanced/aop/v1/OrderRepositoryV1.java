48022be4753012b2cabf148b4f59c279470fb492
bsh6463 <bsh9510@gmail.com>
2021-11-19 23:48:26 +0900

    211119 Log Tracer V1

---
+++ src/main/java/hello/advanced/aop/v1/OrderRepositoryV1.java
@@ -0,0 +1,44 @@
+package hello.advanced.aop.v1;
+
+import hello.advanced.hellotrace.HelloTraceV1;
+import hello.advanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId){
+
+        TraceStatus status = null;
+
+        try{
+            status = trace.begin("OrderRepository.save()");
+
+            //
+            if (itemId.equals("ex")){
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+            //
+
+            trace.end(status);
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e; //예외를 반드시 다시 던져줘야 함.
+        }
+
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
