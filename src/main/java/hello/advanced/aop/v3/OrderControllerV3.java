acf3d0bea906e5db6df2e588e6899bfa0da82746
bsh6463 <bsh9510@gmail.com>
2021-11-20 14:09:20 +0900

    211120 Filed Synchronization

---
+++ src/main/java/hello/advanced/aop/v3/OrderControllerV3.java
@@ -0,0 +1,32 @@
+package hello.advanced.aop.v3;
+
+import hello.advanced.hellotrace.HelloTraceV2;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@Controller +@ResponseBody
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId){
+
+        TraceStatus status = null;
+
+        try{
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e; //예외를 반드시 다시 던져줘야 함.
+        }
+    }
+}
