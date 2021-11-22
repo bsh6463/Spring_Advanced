913508079eb66a367a6d757a5dba77fd5320e302
bsh6463 <bsh9510@gmail.com>
2021-11-22 20:58:25 +0900

    211122 Strategy Patter 3

--- src/test/java/hello/advanced/trace/strategy/ContextV1Tet.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Tet.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -60,5 +61,67 @@ public class ContextV1Tet {
         contextV2.execute();
     }
 
+    /**
+     * 익명 내부 클래스
+     */
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy() {
 
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        ContextV1 context = new ContextV1(strategyLogic1);
+        context.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+
+    @Test
+    void strategyV3(){
+
+
+        ContextV1 context = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        context.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+
+    @Test
+    void strategyV4(){
+
+        ContextV1 context = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
 }
