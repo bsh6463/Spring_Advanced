1fc79afe805bc645a9a3378b4dde3fc69cceb2e7
bsh6463 <bsh9510@gmail.com>
2021-11-22 19:24:37 +0900

    211122 TemplateMethod example3

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -60,5 +60,28 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodV2(){
+
+        //상속 받으면서 구현 바로 함.
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                 log.info("비즈니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                 log.info("비즈니스 로직 2 실행");
+            }
+        };
+        template2.execute();
+        log.info("클래스 이름2 = {}", template2.getClass());
+    }
+
 
 }
