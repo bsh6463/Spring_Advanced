6fc43259847e9c46ea71d86556e5c32eec39bf1a
bsh6463 <bsh9510@gmail.com>
2021-11-22 14:05:04 +0900

    211122 Concurrency Issue

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,57 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field(){
+        log.info("main start");
+
+        /*
+        Runnable runnable= new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }*/
+
+        //Ruunable 인터페이스의 run() 구현.
+        //Thread는 Thread 클래스를 상속 받거나, Runnable 인터페이스를 구현하거나,
+        //Runnable 인터페이스 구현 후 Thread에 주입하는 방식으로 사용 가능.
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        //sleep(2000); //동시성 문제 발생 안하는 조건.
+        sleep(100); //동시성 문제 발생하는 조건.
+        threadB.start();
+
+        sleep(2000); //메인 Thread 종료 대기
+        log.info("main exit");
+
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
